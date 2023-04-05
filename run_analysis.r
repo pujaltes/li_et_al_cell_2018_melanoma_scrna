2fdec6804d457b1f11a06616716363a6127c4e44
pujaltes <seby131@gmail.com>
2023-04-05 17:06:20 +0100

    new script and gitignore .Rou

---
+++ run_analysis.r
@@ -0,0 +1,11 @@
+# to get file output run with: R CMD BATCH run_analysis.r
+# Loading code and downloading required data files
+library("tgstat")
+source("pipe.r")
+
+
+# Building the metacells
+build_metacells()
+
+# Generate figures
+generate_figs()
